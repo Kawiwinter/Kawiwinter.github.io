8500718703304bf07eac474a0b174b0403ce09a7
Kawi <kawiwinter@gmail.com>
2022-05-21 18:37:56 +0200

    finetuned error handling

--- script.js
+++ script.js
@@ -4,15 +4,60 @@ const button = document.querySelector(".button");
 const colorbox = document.querySelector(".colorbox");
 const userinput = document.querySelector(".hex");
 
+const notHexa = [
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "X",
+  "Y",
+  "Z",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z",
+];
+
 button.addEventListener("click", () => {
+  const userinputValue = userinput.value;
+  const inputChars = userinputValue.split("");
   if (
-    userinput.value.length === 6 &&
-    userinput.value.matches("[a-fA-F *\t]+")
+    userinputValue.length === 6 &&
+    !notHexa.some((c) => inputChars.includes(c))
   ) {
-    colorbox.style.backgroundColor = `#${userinput.value}`;
+    colorbox.style.backgroundColor = `#${userinputValue}`;
     userinput.style.borderColor = "";
   } else {
     userinput.style.borderColor = "red";
-    alert("Value must be 6 long and between 000000 and FFFFFF");
+    alert(
+      "Invalid hex code! Value must be 6 long and between 000000 and FFFFFF"
+    );
   }
 });
