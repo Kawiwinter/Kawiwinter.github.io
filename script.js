595e25f0236adfa2cbd8f6f08d14734d084c4f45
Kawi <kawiwinter@gmail.com>
2022-05-21 18:03:12 +0200

    more error handling

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const colorbox = document.querySelector(".colorbox");
 const userinput = document.querySelector(".hex");
 
 button.addEventListener("click", () => {
-  if (userinput.value.length === 6) {
+  if (
+    userinput.value.length === 6 &&
+    userinput.value.matches("[a-fA-F *\t]+")
+  ) {
     colorbox.style.backgroundColor = `#${userinput.value}`;
     userinput.style.borderColor = "";
   } else {
